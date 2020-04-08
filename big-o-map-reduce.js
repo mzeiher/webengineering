2bfea544b7de0d0c57d172e16348e4fd965020e8
Mathis Zeiher <mathis.zeiher@gmail.com>
2020-04-08 10:29:26 +0200

    Create big-o-map-reduce.js

---
+++ big-o-map-reduce.js
@@ -0,0 +1,20 @@
+const numbers = [];
+for(let i = 0; i < 1000000; i++) {
+  //numbers.push(Math.round(Math.random() * 2)+1);
+  //numbers.push(2);
+  numbers.push(1);
+}
+const filter = (value) => value % 2 === 0;
+const map = (value) => value*value;
+const reduce = (acc, value) => acc + value;
+
+//
+numbers.filter(filter).map(map).reduce(reduce,0)
+
+//
+let acc = 0;
+for(let num of numbers) {
+  if(filter(num)) {
+    acc = reduce(acc, map(num));
+  }
+}
