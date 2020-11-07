1e49d4dc7aae993565205058c4e13eae5b94dc90
Mathis Zeiher <mathis.zeiher@gmail.com>
2020-11-07 15:38:23 +0100

    updated exhoserver

--- tools/echoserver/echoserver.js
+++ tools/echoserver/echoserver.js
@@ -1,13 +1,19 @@
 const http = require('http');
 const Busboy = require('busboy');
 const inspect = require('util').inspect;
-const crypto = require('crypto');
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ noServer: true });
+
+const clients = [];
 
 process.on('SIGINT', () => {
     process.exit(0);
 })
 
 const server = http.createServer((req, res) => {
+    
     if (/^\/post$/.test(req.url) && req.method === 'POST') {
         if(req.headers["content-type"].startsWith('multipart/form-data')) {
             res.writeHead(405, '405 Method Not Allowed');
@@ -66,24 +72,30 @@ WS:/ws will create a ws echo service (not yet working)
 });
 
 server.on('upgrade', (req, socket, head) => {
-    if (/^\/ws$/.test(req.url)) {
-
-        const key = req.headers['sec-websocket-key'];
-        const sha = crypto.createHash('sha1');
-        sha.update(key + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11', 'utf8');
-        const result = sha.digest('base64');
-        socket.write('HTTP/1.1 101 Web Socket Protocol Handshake\r\n' +
-            'Upgrade: WebSocket\r\n' +
-            'Connection: Upgrade\r\n' +
-            'Sec-WebSocket-Accept: '+ result + '\r\n' +
-            '\r\n');
-
-        // to be done -> masking etc...
+    
+    if (/^\/ws(\?.*)?$/.test(req.url)) {
+        wss.handleUpgrade(req, socket, head, (client) => {
+            
+            clients.push(client);
+            client.on('close', function() {
+                clients = clients.filter((value) => value !== client);
+            });
+            client.on('error', function() {
+                clients = clients.filter((value) => value !== client);
+            });
+            client.on('message', (data) => {
+                clients.forEach(client => {
+                    client.send(data);
+                })
+            });
+            wss.emit('connection', client, req);
+        });
     } else {
         socket.write('HTTP/1.1 404 Not Found\r\n')
+        socket.destroy();
     }
 });
 
 server.listen(8090, '0.0.0.0', () => {
-    console.log('listening on 8081');
+    console.log('listening on 8090');
 })
