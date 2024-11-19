e4099997f551acd78f63905d40e9493d3e4f79e0
Mathis Zeiher <mathis.zeiher@gmail.com>
2024-11-19 15:21:49 +0100

    chore: added example for lit-html with backend

--- exercises/javascript/lit-html/todo.js
+++ exercises/javascript/lit-html/todo.js
@@ -3,28 +3,68 @@ import {
   render,
 } from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
 
-let todos = ["init"];
+class Store extends EventTarget {
+  constructor() {
+    super();
+    const ws = new WebSocket("/");
+    ws.addEventListener("message", (evt) => {
+      this.dispatchEvent(
+        new CustomEvent("update", { detail: JSON.parse(evt.data) }),
+      );
+    });
+  }
 
-function update() {
+  addTodo(todo) {
+    fetch("/todos", { method: "POST", body: todo });
+  }
+
+  deleteTodo(idx) {
+    fetch(`/todos/${idx}`, { method: "DELETE" });
+  }
+
+  get() {
+    fetch("/todos", { method: "GET" })
+      .then((value) => {
+        return value.json();
+      })
+      .then((value) => {
+        this.dispatchEvent(new CustomEvent("update", { detail: value }));
+      });
+  }
+}
+
+const store = new Store();
+store.addEventListener("update", (evt) => {
+  update(evt.detail);
+});
+store.get();
+
+function update(todos) {
   render(
     html`<div>
       <form
         @submit=${(evt) => {
           evt.preventDefault();
-          const input = document.querySelector("[name=text]");
-          todos.push(input.value);
-          update();
+          store.addTodo(evt.target.elements[0].value);
         }}
       >
         <input type="text" name="text" /><button>Add</button>
       </form>
       <ul>
-        ${todos.map((value) => {
-          return html`<li>${value}</li>`;
+        ${todos.map((value, idx) => {
+          return html`<li>
+            <label> ${value}</label
+            ><button
+              @click=${() => {
+                store.deleteTodo(idx);
+              }}
+            >
+              X
+            </button>
+          </li>`;
         })}
       </ul>
     </div>`,
     document.querySelector("body"),
   );
 }
-update();
